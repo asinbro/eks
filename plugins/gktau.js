d5f26348d7afaedf2af7c11a31941a04644171ef
asinbro <generatorpush2@gmail.com>
2023-02-08 22:06:55 +0700

    eks

---
+++ plugins/gktau.js
@@ -0,0 +1,14 @@
+//let { MessageType } = require('@adiwajshing/baileys')
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+let handler = async (m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
+  m.reply(text + 'tiktok.com/@asinyasinzz', null, {
+    contextInfo: { mentionedJid: users }
+  })
+}
+handler.command = ['zyko-md']
+
+handler.admin = false
+handler.group = false
+
+export default handler
